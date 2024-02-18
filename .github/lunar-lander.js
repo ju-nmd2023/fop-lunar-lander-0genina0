ee6da0e033548d41e4704b5e0e6a340f6030d520
0genina0 <geni23cs@student.ju.se>
2024-02-18 19:26:55 +0100

    adding moon

--- .github/lunar-lander.js
+++ .github/lunar-lander.js
@@ -1,12 +1,31 @@
-
-
 let starX = [];
 let starY = [];
 let starAlpha = [];
 
+function earth(){
+    fill(40,122,184);
+    ellipse(500,100,250);
+}
+
+function moon (){
+    fill(194,197,204);
+    ellipse(300,710,600); 
+
+    fill("darkgrey");
+    ellipse(450,500,30, 25); 
+    ellipse(380,460,20);
+    ellipse(110,520,17, 15);
+    stroke("lightgrey");
+    strokeWeight(9);
+
+    ellipse(190,520,100,80);
+    noStroke();
+}
+
 function skull(){
     //skull
-    translate(150,200);
+    scale(0.5); 
+    translate(450,520);
     noStroke();
     fill(255,255,255);
     ellipse(151,75,45);
@@ -23,32 +42,42 @@ function skull(){
     line(148,104,148,95);
     line(154,104,154,95);
     line(160,104,160,95);
+
+    //window
+    fill(173,216,230,160);
+    noStroke();
+    ellipse(150,80,105);
     }
 
     //rocket
 function rocket(){
+    fill(87,88,87);
+    arc(300,400,60,260,PI,0,CHORD);
+
     fill("darkgrey");
     arc(300,385,80,200,PI,0,CHORD);
 
     fill(150,123,182);
    arc(300,360,100,260,PI,0,CHORD);
 
-   fill("lightblue");
+   fill(35,46,48);
    stroke("darkgrey");
-   strokeWeight(8);
+    strokeWeight(8);
    ellipse(300,300,60);
 
    noStroke();
    fill(157,193,131);
    triangle(273,250,327,250,299,205);
+
+
 }
 
 function flame(){
     fill("orange");
-    ellipse(300,390,50);
+    ellipse(300,400,50);
 
     fill("yellow");
-    ellipse(300,390,30);
+    ellipse(300,400,30);
 }
 
 for(let i= 0; i < 500; i++){
@@ -71,8 +100,11 @@ function draw (){
     starAlpha[index] = starAlpha[index] + 0.04;
     }
     
+    earth();
+    moon();
     flame();
     rocket();
-
+    skull();  
+  
   
 }
